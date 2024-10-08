5577cb91180e0ca86a3a4a353f90481442ab2732
Connor Schicht <connor.schicht@gmail.com>
2024-10-08 14:06:03 +1100

    installed package removed old code and added css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,7 @@
-import Header from "./components/Header";
-import PostList from "./components/PostList";
-import Footer from "./components/Footer";
-
 function App() {
   return (
     <div>
-      <Header />
-      <PostList />
-      <Footer />
+      <p>Hello World</p>
     </div>
   );
 }
